cd25e635247757ad9cb787abeb2ed071efac6928
Tayyab-athar <58460621+Tayyab-athar@users.noreply.github.com>
2020-06-03 14:03:15 +0500

    renaming

--- usr/local/lib/web/compile_java.sh
+++ usr/local/lib/web/compile_java.sh
@@ -1,2 +1,1 @@
 echo "Initializing process..." && export DISPLAY=:1.0 && cd /usercode && rm -f *.class && (pkill java || true) && echo "Compiling..." && javac HelloWorldSwing.java && echo "Executing Program..." && java -Dsun.java2d.xrender=false HelloWorldSwing
-
