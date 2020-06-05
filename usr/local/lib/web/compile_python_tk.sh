644cc4e1c19098c592681b103ad752b2d9b0733e
Tayyab-athar <58460621+Tayyab-athar@users.noreply.github.com>
2020-06-05 18:47:53 +0500

    modifying python tk compilation file

--- usr/local/lib/web/compile_python_tk.sh
+++ usr/local/lib/web/compile_python_tk.sh
@@ -1,1 +1,13 @@
-echo "Initializing process..." && export DISPLAY=:1.0 && cd /usercode && echo "Executing Program..." && python3.8 HelloWorldTkinker.py
+echo "Initializing process..."
+export DISPLAY=:1.0 
+FILE=/usercode/pid.txt
+if test -f "$FILE"; then
+    read -r PID<$FILE
+    if ps -p $PID > /dev/null;then
+          kill -9 $PID
+    fi
+fi
+cd /usercode 
+echo "Executing Program..." 
+python3.8 HelloWorldTkinker.py&
+echo $! >/usercode/pid.txt
