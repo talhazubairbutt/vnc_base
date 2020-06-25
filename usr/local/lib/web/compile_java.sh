4efeb19bb5cd22e7cf1683b9c797f1040db58d75
Talha Zubair Butt <talha.zubair@educative.io>
2020-06-25 12:12:40 +0500

    remove compile scripts

--- usr/local/lib/web/compile_java.sh
+++
@@ -1,1 +0,0 @@
-echo "Initializing process..." && export DISPLAY=:1.0 && cd /usercode && rm -f *.class && (pkill java || true) && echo "Compiling..." && javac HelloWorldSwing.java && echo "Executing Program..." && java -Dsun.java2d.xrender=false HelloWorldSwing
